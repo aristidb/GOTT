52c5d3c5a7018405128884658be0b32f80da49d7
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2005-05-09 17:33:36 +0000

    no longer crashing on test

--- gott/util/tdl/test/schema/multi_footype.cpp
+++ gott/util/tdl/test/schema/multi_footype.cpp
@@ -36,6 +36,7 @@ typedef schema::rule::attributes RA;
 
 namespace {
 struct schema_multi_footype : tut::schema_basic {
+  schema::context c[2];
   schema_multi_footype() {
     schema::context_template document, footype, multi;
 
@@ -60,7 +61,6 @@ struct schema_multi_footype : tut::schema_basic {
       multi.end();
     multi.end();
 
-    schema::context c[2];
     std::vector<schema::context*> v(2);
     for (size_t i = 0; i < 2; ++i)
       v[i] = &c[i];
@@ -82,72 +82,5 @@ namespace {
 }
 
 namespace tut {
-template<> template<>
-void object::test<1>() {
-  run_test(L"4\n 5");
-  stru::cf::nd_list c;
-  C(M(c, L"foll"), L"doc").write_to(xp);
-  ensure_equals("single multi_footype entity", tree, xp);
+// ACTUALLY TEST (TODO)
 }
-
-template<> template<>
-void object::test<2>() {
-  try {
-    run_test(L"d7");
-    fail("just string");
-  } catch (schema::mismatch const &mm) {
-    ensure_equals("correct error", 
-        std::string(mm.what()), "1:1 : mismatch at token d7");
-  }
-}
-
-template<> template<>
-void object::test<3>() {
-  try {
-    run_test(L"");
-    fail("empty");
-  } catch (schema::mismatch const &mm) {
-    ensure_equals("correct error", 
-        std::string(mm.what()), "0:1 : mismatch after token ");
-  }
-}
-
-template<> template<>
-void object::test<4>() {
-  try {
-    run_test(L"-77 foo");
-    fail("followed string");
-  } catch (schema::mismatch const &mm) {
-    ensure_equals("correct error", 
-        std::string(mm.what()), "1:5 : mismatch at token foo");
-  }
-}
-
-template<> template<>
-void object::test<5>() {
-  try {
-    run_test(L"4");
-    fail("just one integer");
-  } catch (schema::mismatch const &mm) {
-    ensure_equals("correct error", 
-        std::string(mm.what()), "1:1 : mismatch after token 4");
-  }
-}
-
-template<> template<>
-void object::test<6>() {
-  try {
-    run_test(L"4 99,y");
-  } catch (schema::mismatch const &mm) {
-    ensure_equals("correct error", 
-        std::string(mm.what()), "1:6 : mismatch at token y");
-  }
-}
-
-// further tests
-}
-
-/*
-
-
-*/
