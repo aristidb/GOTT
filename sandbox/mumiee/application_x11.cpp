605c6ed40ef6b5cdd6e9277057100872fe6f2029
Andreas Pokorny <diemumiee@gmx.de>
2005-05-17 20:38:04 +0000

    particularly fixing window behaviour redraw events should now be fired properly

--- sandbox/mumiee/application_x11.cpp
+++ sandbox/mumiee/application_x11.cpp
@@ -8,24 +8,40 @@ namespace gott{namespace gui{
   
 application::status application::handle_pending_messages()
 {
-  while( XPending( global_data.connection ) > 0 )
+  if( process_idle.num_slots() )
+  {
+    while( XPending( global_data.connection ) > 0 )
+    {
+      XEvent					event;
+      XNextEvent( global_data.connection, &event );
+
+      window * win = global_data.decode_window_object( event.xany.window );
+
+      if( win== 0 )
+      {
+        std::cerr << "Unable to decode window" << std::endl;
+        break;
+      }
+
+      // handle this message
+      global_data.process_event( win, event ); // RETURN VALUE?
+    }
+    XSync( global_data.connection, 0 );
+  }
+  else 
   {
     XEvent					event;
     XNextEvent( global_data.connection, &event );
 
     window * win = global_data.decode_window_object( event.xany.window );
 
-    if( win== 0 )
-    {
+    if( win )
+      global_data.process_event( win, event ); 
+    else 
       std::cerr << "Unable to decode window" << std::endl;
-      break;
-    }
-
-    // handle this message
-    global_data.process_event( win, event ); // RETURN VALUE?
   }
+
   
-  XSync( global_data.connection, 1 );
   return windows.empty() ? End : Continue; 
 }
 
