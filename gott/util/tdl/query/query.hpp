14a59c23d23f32e230b5e71cecbc5510cd4633e5
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2005-06-14 19:32:10 +0000

    Even even even more doxygen.

--- gott/util/tdl/query/query.hpp
+++ gott/util/tdl/query/query.hpp
@@ -28,18 +28,59 @@ namespace util {
 namespace tdl {
 namespace query {
 
+/**
+ * A set of iterators of a structure::tree::iterator compatible type.
+ */
 template<class Iterator> class selection {
 public:
+  /// Create an empty selection.
   selection() {}
+  
+  /**
+   * Create a selection with a single element.
+   * \param i The selection's content.
+   */
   explicit selection(Iterator const &i) : impl(1, i) {}
+
+  /**
+   * Create a selection from a STL-compatible iterator range.
+   * \param a An iterator to the beginning of the range.
+   * \param b An iterator just after the end of the range.
+   */
   template<class I> selection(I a, I b) : impl(a, b) {}
 
-  selection const &operator+=(selection const &) EXPORT;
+  /**
+   * Add another selection to this selection.
+   * \param o The selection to add.
+   */
+  selection const &operator+=(selection const &o) EXPORT;
 
+  /**
+   * From every iterator, get an index-specific child.
+   * \param i The children's index.
+   * \return The new selection with the children.
+   */
   selection find(std::size_t i) const EXPORT;
+
+  /**
+   * From every iterator, get a range of index-specific children.
+   * \param start The children's minimal (inclusive) index.
+   * \param stop The children's maximal (inclusive) index.
+   * \return The new selection with the children.
+   */
   selection find_range(std::size_t start, std::size_t stop) const EXPORT;
+
+  /**
+   * Gets all children from the iterators.
+   * \return The new selection with the children.
+   */
   selection find_all() const EXPORT;
-  selection find_tag(std::wstring const &) const EXPORT;
+
+  /** 
+   * Gets all children from the iterators with a specific tag.
+   * \param tag The tag to search for.
+   */
+  selection find_tag(std::wstring const &tag) const EXPORT;
 
 private:
   typedef std::vector<Iterator> container;
@@ -52,6 +93,7 @@ public:
   const_iterator end() const { return impl.end(); }
   size_type size() const { return impl.size(); }
 
+  /// Compare two selections for equality.
   bool operator==(selection const &) const EXPORT;
 
 private:
@@ -63,11 +105,13 @@ private:
   void add_wtag(Iterator const &, std::wstring const &);
 };
 
+/// Checks whether two selections differ.
 template<class I>
 bool operator!=(selection<I> const &a, selection<I> const &b) {
   return !(a == b);
 }
 
+/// Prints a selection to a stream.
 template<class Ch, class ChT, class I>
 std::basic_ostream<Ch, ChT> &operator<<(std::basic_ostream<Ch, ChT> &s,
                                         selection<I> const &x) {
