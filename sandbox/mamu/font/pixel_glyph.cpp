ad72fb175e8c6e3f71fcbfccf8d418d79741bc35
Andreas Pokorny <diemumiee@gmx.de>
2005-07-07 21:35:31 +0000

    * add files device, pixel_glyph.hpp pixel_glyph.cpp * test call for pixel glyphs * pixel font implementation based on glDrawPixels does not yet work.

---
+++ sandbox/mamu/font/pixel_glyph.cpp
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <stdexcept>
+#include <iostream>
+#include <cstdlib>
+#include <GL/glu.h>
+#include <freetype/ftoutln.h>
+#include "pixel_glyph.hpp"
+
+using namespace std;
+
+namespace gott{namespace gui{namespace font{
+
+
+pixel_glyph::pixel_glyph( FT_Face & face, std::size_t glyph_index, device_property const& d ) 
+{
+
+  int error;
+/*  switch( d.dot_layout ) {
+    case LCDVertical: error =FT_Load_Glyph( face, glyph_index, FT_LOAD_RENDER | FT_LOAD_TARGET_LCD_V );break;
+    case LCDHorizontal: error=FT_Load_Glyph( face, glyph_index, FT_LOAD_RENDER | FT_LOAD_TARGET_LCD );break;
+    case CRTDiffuse: 
+    default: error=FT_Load_Glyph( face, glyph_index, FT_LOAD_RENDER );break;
+  };*/
+  error=FT_Load_Glyph( face, glyph_index, FT_LOAD_RENDER );
+  if(error)
+    throw runtime_error("Fehler bei FT_Set_Char_Size");
+
+  FT_Bitmap & bitmap = face->glyph->bitmap;
+
+  size_t width = bitmap.width;
+  size_t height = bitmap.rows;
+  unsigned char *buffer = new unsigned char[ ((height - face->glyph->bitmap_top) + height )* width * 2];
+  std::memset( buffer, 0 , width*height*2 );
+  for( std::size_t i = 0; i < height; ++i ) {
+    for(  std::size_t j = 0; j < width; ++j ) {
+      if(  bitmap.buffer[i*width+ j] ) {
+        buffer[((height-1-(i + (height - face->glyph->bitmap_top)))*width + j)*2 ] = 255;
+        buffer[((height-1-(i + (height - face->glyph->bitmap_top)))*width + j)*2 + 1 ] = bitmap.buffer[i*width+ j];
+      }
+    }
+  }
+  list = glGenLists( 1 );
+  glPixelStorei( GL_UNPACK_SWAP_BYTES, false );
+  glPixelStorei( GL_UNPACK_LSB_FIRST, false );
+  glPixelStorei( GL_UNPACK_ALIGNMENT, 1 );
+  glPixelStorei( GL_UNPACK_IMAGE_HEIGHT , 0 );
+  glPixelStorei( GL_UNPACK_SKIP_PIXELS, 0 );
+  glPixelStorei( GL_UNPACK_SKIP_ROWS , 0 );
+  glPixelStorei( GL_UNPACK_ROW_LENGTH, 0 );
+  glNewList(list, GL_COMPILE);
+  glDrawPixels(  width, height,  GL_LUMINANCE_ALPHA , GL_UNSIGNED_BYTE, buffer );
+  glEndList();
+}
+
+pixel_glyph::~pixel_glyph(){ glDeleteLists( list, 1 ); }
+
+void pixel_glyph::render() const{
+  glCallList(list);
+}
+
+}}}
