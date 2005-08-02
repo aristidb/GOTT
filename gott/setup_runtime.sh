480c571f337c56b2751579542d9a99043647d8a5
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2005-08-02 18:29:20 +0000

    Fixed setup_runtime.sh

--- gott/setup_runtime.sh
+++ gott/setup_runtime.sh
@@ -1,3 +1,3 @@
 # source me!
-export LD_LIBRARY_PATH=$(find $PWD -name lib*.so|sed -e 's/\/[^/]*$//')
+export LD_LIBRARY_PATH=$(find $PWD -name 'lib*.so'|sed -e 's/\/[^/]*$//'|xargs -i echo -n {}:)$LD_LIBRARY_PATH
 echo $LD_LIBRARY_PATH
