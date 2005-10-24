56b7a4f4c731fa25d3a745cc8ef8caa2941bbbff
Rüdiger Sonderfeld <ruediger@c-plusplus.de>
2005-10-24 14:55:18 +0000

    XDamage

--- sandbox/kingruedi/x11/xdamage0.c
+++ sandbox/kingruedi/x11/xdamage0.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/extensions/Xdamage.h>
 
 enum { WIDTH=200,
        HEIGHT=200 };
+
+#define MAX(a, b) ((a)>(b) ? (a) : (b))
+#define MIN(a, b) ((a)>(b) ? (b) : (a))
+
+bool rect_intersect(XRectangle const *a, XRectangle const *b) {
+  short a_w=a->x + a->width;
+  short a_h=a->y + a->height;
+  short b_w=b->x + b->width;
+  short b_h=b->y + b->height;
+
+  if(MIN(a_w, b_w) - MAX(a->x, b->x) > 0 &&
+     MIN(a_h, b_h) - MAX(a->y, b->y) > 0)
+    return true;
+  return false;
+}
 
 int main()
 {
@@ -40,36 +56,43 @@ int main()
   XMapWindow(display, window);
   XFlush(display);
 
+  enum { rects_n=(WIDTH*HEIGHT)/20 };
+  XRectangle rects[rects_n];
+
+  size_t n=0;
+  for(unsigned int x=0; x<WIDTH; x+=20) {
+    for(unsigned int y=0; y<HEIGHT; y+=20) {
+      rects[n].x=x+1;
+      rects[n].y=y+1;
+      rects[n].width=rects[n].height=18;
+      XDrawRectangle(display, window, gc, rects[n].x, rects[n].y,
+		     rects[n].width, rects[n].height);
+      //XserverRegion damage_r=XFixesCreateRegion(display, &rects[n], 1);
+      //XDamageSubtract(display, damage, None, damage_r);
+      ++n;
+    }
+  }
+
   while(1){
     XEvent event;
     XNextEvent(display, &event);
 
-    switch(event.type){
-    case Expose:
-      if(event.xexpose.count > 0)
-	break;
-      for(unsigned int x=0; x<WIDTH; x+=20) {
-	for(unsigned int y=0; y<HEIGHT; y+=20) {
-	  XRectangle rect;
-	  rect.x=x+1;
-	  rect.y=y+1;
-	  rect.width=rect.height=18;
-	  XDrawRectangle(display, window, gc, rect.x, rect.y,
-			 rect.width, rect.height);
-	  //	  XserverRegion damage_r=XFixesCreateRegion(display, &rect, 1);
-	  //	  XDamageSubtract(display, damage, None, damage_r);
-	}
-      }
-      break;
-    case KeyPress: 
+    if(event.type == KeyPress) {
       XDamageDestroy(display, damage);
       XCloseDisplay(display); 
       return 0;
     }
-    if(event.type == xdamage_event_base+XDamageNotify) {
+    else if(event.type == xdamage_event_base+XDamageNotify) {
       XDamageNotifyEvent *notify=(XDamageNotifyEvent *)&event;
-      printf("Damage: (%d,%d)(%d,%d)\n",notify->area.x, notify->area.y,
+      printf("Damage: (%d,%d)(%d,%d)\n", notify->area.x, notify->area.y,
 	     notify->area.width, notify->area.height);
+
+      for(size_t n; n<rects_n; ++n) {
+	if(rect_intersect(&rects[n], &notify->area))
+	  XDrawRectangle(display, window, gc, rects[n].x, rects[n].y,
+			 rects[n].width, rects[n].height);
+      }
+      XFlush(display);
     }
   }
   return 1;
