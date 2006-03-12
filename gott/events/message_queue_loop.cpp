e134f2ea47e43ec602dc53436302fa9ff896ad4c
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2006-03-12 00:50:53 +0000

    Events: * added new, pretty restricted and maybe bogus kind of main_loop

---
+++ gott/events/message_queue_loop.cpp
@@ -0,0 +1,68 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is An Event Handling Class Library.
+ *
+ * The Initial Developer of the Original Code is
+ * Aristid Breitkreuz (aribrei@arcor.de).
+ * Portions created by the Initial Developer are Copyright (C) 2006
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *  Aristid Breitkreuz (aribrei@arcor.de)
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+#include "message_queue_loop.hpp"
+
+using gott::events::message_queue_loop;
+using gott::xany::Xany;
+
+message_queue_loop::message_queue_loop() 
+  : running(false), sig_mgr(this) {}
+message_queue_loop::~message_queue_loop() {}
+
+void message_queue_loop::quit_local() {
+  running = false;
+}
+
+void message_queue_loop::run() {
+  running = true;
+  while (running) {
+    Xany val = queue.pop();
+    on_receive_.emit(val);
+  }
+}
+
+void message_queue_loop::send(Xany const &val) {
+  queue.push(val);
+}
+
+void *message_queue_loop::do_feature(gott::QID const &qid) {
+  GOTT_EVENTS_FEATURE(qid,inprocess_message_manager);
+  if (qid == signal_manager::qid)
+    return &sig_mgr;
+  return 0;
+}
