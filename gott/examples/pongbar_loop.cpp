e75a315eca34b9f96d676d2854f9a8ff009e82be
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2006-04-11 22:02:58 +0000

    Examples: * fixed pongbar_loop example and improved readability

--- gott/examples/pongbar_loop.cpp
+++ gott/examples/pongbar_loop.cpp
@@ -2,39 +2,42 @@
 #include <gott/events/main_loop.hpp>
 #include <gott/events/timer_manager.hpp>
 #include <gott/events/quit_manager.hpp>
-#include <boost/lambda/lambda.hpp>
-#include <boost/lambda/bind.hpp>
-#include <boost/lambda/if.hpp>
 #include <boost/scoped_ptr.hpp>
 #include <iostream>
 
 using namespace gott::events;
-using namespace boost::lambda;
 
 const int width = 80;
 
-void draw_bar(std::ostream &out, int pos) {
-  const int width = 80;
-  out << '|';
-  for (int i = 0; i < width - 2; ++i)
+void draw_bar(int pos) {
+  std::cout << "\r|";
+  for (int i = 0; i <= width - 2; ++i)
     if (i == pos)
-      out << '*';
+      std::cout << '*';
     else
       std::cout << ' ';
-  out << "|\r" << std::flush;
+  std::cout << "|\r" << std::flush;
 }
+
+class bar_drawer {
+  int pos;
+  int dx;
+public:
+  bar_drawer() : pos(0), dx(1) {}
+
+  void operator() () {
+    draw_bar(pos);
+    if (pos + dx == -1 || pos + dx == width - 1)
+      dx = -dx;
+    pos += dx;
+  }
+};
 
 int main() {
   boost::scoped_ptr<main_loop> loop(new select_loop);
-  int i = 0;
-  int dx = 1;
   loop->feature<timer_manager>().add_timer(periodic_timer(
-        boost::posix_time::milliseconds(100),
-        (
-         bind(&draw_bar, var(std::cout), var(i)),
-         var(i) += var(dx),
-         if_then((var(i) == 0 || var(i) == width - 2), var(dx) = -var(dx))
-        )));
+        boost::posix_time::milliseconds(80),
+        bar_drawer()));
   loop->feature<quit_manager>().enable_master();
   loop->run();
   std::cout << std::endl;
