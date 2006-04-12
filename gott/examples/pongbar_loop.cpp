a2c4534a11087c0fdf6cb4f7dfe3ecf9631ce713
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2006-04-12 22:59:23 +0000

    Examples: * vastly expanded pongbar_loop example (three blinking bars)

--- gott/examples/pongbar_loop.cpp
+++ gott/examples/pongbar_loop.cpp
@@ -3,52 +3,131 @@
 #include <gott/events/timer_manager.hpp>
 #include <gott/events/quit_manager.hpp>
 #include <boost/scoped_ptr.hpp>
+#include <boost/lambda/bind.hpp>
+#include <boost/lambda/lambda.hpp>
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
 #include <ctime>
 
 using namespace gott::events;
+using namespace boost::lambda;
+using namespace boost::posix_time;
 
-const int width = 60;
-
-void draw_bar(int pos) {
-  std::cout << "\r|";
-  for (int i = 0; i <= width - 2; ++i)
-    if (i == pos)
-      std::cout << '*';
-    else
-      std::cout << ' ';
-  std::cout << '|';// << std::flush;
-}
+class bar {
+public:
+  virtual void draw() = 0;
+  virtual ~bar() {}
+};
 
 class bar_drawer {
+public:
+  void add(bar *b) {
+    bars.push_back(b);
+  }
+
+  void redraw() {
+    std::cout << '\r';
+    std::for_each(bars.begin(), bars.end(), bind(&bar::draw, _1));
+    std::cout << std::flush;
+  }
+
+private:
+  std::vector<bar *> bars;
+};
+
+class pong_bar : public bar {
+public:
+  pong_bar(int width, bar_drawer &bd) 
+  : width(width), pos(0), dx(1), token('*'), drawer(bd) {
+    drawer.add(this);
+  }
+
+  void pong_step() {
+    advance();
+    drawer.redraw();
+  }
+
+  void hop() {
+    pos = (std::rand() % (width - 2));
+    drawer.redraw();
+  }
+
+  void toggle_token() {
+    if (token == '*')
+      token = '.';
+    else
+      token = '*';
+    drawer.redraw();
+  }
+
+  void draw() {
+    std::cout << '|';
+    for (int i = 0; i < width - 2; ++i)
+      if (i == pos)
+        std::cout << token;
+      else
+        std::cout << ' ';
+    std::cout << '|';
+  }
+
+private:
+  void advance() {
+    if (pos + dx == -1 || pos + dx == width - 2)
+      dx = - dx;
+    pos += dx;
+  }
+  
+private:
+  int width;
   int pos;
   int dx;
-  std::clock_t last;
+  char token;
+  bar_drawer &drawer;
+};
+
+class space_bar : public bar {
 public:
-  bar_drawer() : pos(0), dx(1), last(std::clock()) {}
-
-  bool operator() () {
-    draw_bar(pos);
-    if (pos + dx == -1 || pos + dx == width - 1)
-      dx = -dx;
-    pos += dx;
-    
-    std::clock_t next = clock();
-    std::cout << ' ';
-    std::cout.width(6);
-    std::cout << (next - last) << std::flush;
-    last = next;
-
-    return true;
+  space_bar(int width, bar_drawer &d) : width(width) {
+    d.add(this);
   }
+  
+  void draw() {
+    for (int i = 0; i < width; ++i)
+      std::cout << ' ';
+  }
+
+private:
+  int width;
 };
 
 int main() {
+  std::srand(std::time(0));
+  bar_drawer d;
+  pong_bar b1(30, d);
+  space_bar s1(2, d);
+  pong_bar b2(20, d);
+  space_bar s2(2, d);
+  pong_bar b3(26, d);
+  d.redraw();
   boost::scoped_ptr<main_loop> loop(new select_loop);
-  loop->feature<timer_manager>().add_periodic_timer(
-        boost::posix_time::milliseconds(80),
-        bar_drawer(),
-        true);
+  timer_manager &tm = loop->feature<timer_manager>();
+  tm.add_periodic_timer(
+      milliseconds(80),
+      (bind(&pong_bar::pong_step, &b1), true));
+  tm.add_periodic_timer(
+      milliseconds(400),
+      (bind(&pong_bar::toggle_token, &b1),
+       bind(&pong_bar::toggle_token, &b2),
+       bind(&pong_bar::toggle_token, &b3),
+       true));
+  tm.add_periodic_timer(
+      milliseconds(820),
+      (bind(&pong_bar::pong_step, &b2), true));
+  tm.add_periodic_timer(
+      milliseconds(300),
+      (bind(&pong_bar::hop, &b3), true));
   loop->feature<quit_manager>().enable_master();
   loop->run();
   std::cout << std::endl;
