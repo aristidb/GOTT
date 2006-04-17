13f6689d21ad56717af45d71f726cec56bfcf5f8
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2006-04-17 18:03:48 +0000

    Test: * improving fd_manager_loop tests

--- gott/tests/events/fd_manager_loop.cpp
+++ gott/tests/events/fd_manager_loop.cpp
@@ -16,6 +16,7 @@
 #include <gott/events/select_loop.hpp>
 #include <gott/events/kqueue_loop.hpp>
 #include <gott/events/epoll_loop.hpp>
+
 using namespace gott;
 using namespace gott::events;
 
@@ -24,8 +25,12 @@ class fd_manager_loop_test
 {
   CPPUNIT_TEST_SUITE(fd_manager_loop_test);
     CPPUNIT_TEST(test_select_loop_read);
+#ifdef BUILD_EPOLL
     CPPUNIT_TEST(test_epoll_loop);
+#endif
+#ifdef BUILD_KQUEUE
     CPPUNIT_TEST(test_kqueue_loop);
+#endif
   CPPUNIT_TEST_SUITE_END();
 public:
   void setUp() {
@@ -37,24 +42,25 @@ public:
   static void read_event(main_loop *ml, int fd, unsigned mask) {
     CPPUNIT_ASSERT(mask == fd_manager::read);
     char buffer[12];
-    ssize_t n = read_unix(fd, buffer, 12);
+    ssize_t n = read_unix(fd, buffer);
     CPPUNIT_ASSERT(n > 0);
-    CPPUNIT_ASSERT(std::memcmp(buffer, "Hallo Welt!", 12) == 0);
+    CPPUNIT_ASSERT_EQUAL(gott::string("Hallo Welt!"), gott::string(buffer));
     ml->quit_local();
   }
 
   static void write_thread(int fd) {
-    write_unix(fd, "Hallo Welt!", 12);
+    write_unix(fd, "Hallo Welt!");
   }
 
-  void test_select_loop_read() {
+  void test_read(main_loop *ploop) {
+    boost::scoped_ptr<main_loop> loop(ploop);
+
     int pipes[2];
     pipe_unix(pipes);
 
     boost::thread thrd(boost::bind(fd_manager_loop_test::write_thread,
         			   pipes[1]));
 
-    boost::scoped_ptr<main_loop> loop(new select_loop);
     loop->feature<fd_manager>().add_fd
       (pipes[0], fd_manager::read, boost::bind
        (fd_manager_loop_test::read_event, loop.get(), pipes[0], _1));
@@ -63,12 +69,23 @@ public:
     thrd.join();
   }
 
+  void test_select_loop_read() {
+    test_read(new select_loop);
+  }
+
   void test_epoll_loop() {
+#ifdef BUILD_EPOLL
+    test_read(new epoll_loop);
+#endif
   }
 
   void test_kqueue_loop() {
+#ifdef BUILD_KQUEUE
+    test_read(new kqueue_loop);
+#endif
   }
 };
+
 CPPUNIT_TEST_SUITE_REGISTRATION(fd_manager_loop_test);
 
 #endif
