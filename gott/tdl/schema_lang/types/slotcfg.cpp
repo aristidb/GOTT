38d04e3ad811fc5288fe01af502b06c40196acf2
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2006-12-04 16:31:44 +0000

    incomplete

--- gott/tdl/schema_lang/types/slotcfg.cpp
+++ gott/tdl/schema_lang/types/slotcfg.cpp
@@ -37,106 +37,26 @@
  * ***** END LICENSE BLOCK ***** */
 
 #include <gott/plugin.hpp>
+#include <gott/tdl/schema/slot.hpp>
 #include <gott/tdl/schema/match.hpp>
 #include <gott/tdl/schema/rule.hpp>
 #include <gott/tdl/schema/happy_once.hpp>
 #include <gott/tdl/schema/type.hpp>
 #include <gott/tdl/schema/rule_attr.hpp>
 #include <gott/tdl/schema/event.hpp>
-#include <gott/tdl/structure/repatch.hpp>
-#include <boost/shared_ptr.hpp>
 
 using namespace tdl::schema;
-namespace structure = tdl::structure;
-using structure::writable_structure;
-using structure::repatcher_getter_factory;
-using structure::repatcher_getter;
 
 namespace {
 
-struct make_rep_schema {
-  std::vector<rule_t> &out;
-
-  void operator() (repatcher_getter_factory const *rgf) const {
-    out.push_back(
-        rule_one("named",
-          rule_attr(tag = rgf->get_id().get_string(), outer = list()),
-          rgf->parameter_schema()));
-  }
-};
-
-struct rep_rep : structure::concrete_repatcher<rep_rep> {
-  rep_rep(repatcher_getter *helper) : helper(helper) {}
-
-  repatcher_getter *helper;
-
-  writable_structure *deferred_write(writable_structure &) const {
-    struct context : writable_structure {
-      context(repatcher_getter *helper) : helper(helper) {}
-
-      repatcher_getter *helper;
-
-      void begin(tdl::source_position const &w) {
-        helper->begin(w);
-      }
-
-      void end() {
-        helper->end();
-      }
-
-      void add_tag(gott::string const &s) {
-        helper->add_tag(s);
-      }
-
-      void data(gott::Xany const &d) {
-        helper->data(d);
-      }
-    };
-
-    return new context(helper);
-  }
-};
-
 class xmatcher : public happy_once {
 public:
-  xmatcher(
-      rule_attr_t const &attr,
-      std::vector<rule_t> const &children,
-      match &ref)
-  :     happy_once(attr, ref),
-        getter(structure::repatcher_by_name()) {
-    (void)children; //TODO: check that there are _no_ children
-
-    std::vector<rule_t> inner;
-    make_rep_schema helper = { inner };
-    tdl::resource::list<repatcher_getter_factory>(helper);
-    ref.add(rule("unordered",
-          rule_attr(coat = false, repatcher2 = new rep_rep(getter.get())),
-          inner));
-  }
-
-  static bool accept_empty(rule_attr_t const &, std::vector<rule_t> const &) 
-  { return true; }
-
   static gott::atom const id;
 
-private:
-  bool play(ev::child_succeed const &) {
-    matcher().out_structure().begin(matcher().where_out());
-    matcher().out_structure().data(gott::Xany(
-          boost::shared_ptr<structure::repatcher>(            getter->result_alloc())));
-    matcher().out_structure().end();
-    be_happy();
-    return true;
-  }
-
-  gott::string name() const {
-    return id.get_string();
-  }
-
-  rule_t param;
-  boost::scoped_ptr<repatcher_getter> getter;
+  static bool accept_empty(rule_attr_t const &, std::vector<rule_t> const &)
+  { return false; }
 };
+
 }
 
 GOTT_PLUGIN_MAKE_BUILDER_SIMPLE(plugin_lang_repatcher, concrete_type<xmatcher>)
@@ -151,5 +71,5 @@ GOTT_PLUGIN_METADATA(
 "file-path ??FILE??\n"
 )
 
-gott::atom const xmatcher::id("tdl::schema_lang::repatcher");
+gott::atom const xmatcher::id("tdl::schema_lang::slotcfg");
 
